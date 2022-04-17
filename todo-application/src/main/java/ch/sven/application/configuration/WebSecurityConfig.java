0e590f0c4755d74fb349b567ef983cd66d621b9f
Sven Glauser <sven.glauser@divtec.ch>
2022-04-17 19:32:28 +0200

    Petites modifications

--- todo-application/src/main/java/ch/sven/application/configuration/WebSecurityConfig.java
+++ todo-application/src/main/java/ch/sven/application/configuration/WebSecurityConfig.java
@@ -63,15 +63,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static String getRoleRequest() {
         return "SELECT u.username, role.role\n" +
-                "FROM todo.role\n" +
-                "INNER JOIN todo.role_utilisateur ru on role.id = ru.role\n" +
-                "INNER JOIN todo.utilisateur u on u.id = ru.utilisateur\n" +
-                "WHERE u.username = ?";
+               "FROM todo.role\n" +
+               "INNER JOIN todo.role_utilisateur ru on role.id = ru.role\n" +
+               "INNER JOIN todo.utilisateur u on u.id = ru.utilisateur\n" +
+               "WHERE u.username = ?";
     }
 
     private static String getUserRequest() {
         return "SELECT username, password, enabled\n" +
-                "FROM todo.utilisateur\n" +
-                "WHERE username = ?";
+               "FROM todo.utilisateur\n" +
+               "WHERE username = ?";
     }
 }
